90bce3144339626f2b56c2a4244bbec6f13d0a48
jungin4ever <leeji4ever@gmail.com>
2024-03-23 12:36:46 +0900

    Javascript for button

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+You Chose <?php echo $_POST["Q1"]; ?>
+and <?php echo $_POST["Q2"]; ?><br>
+
+
+</body>
+</html>
--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,3 +1,9 @@
 document.querySelector("h2").addEventListener("click", () => {
   alert("Ouch! Stop poking me!");
 });
+
+document.getElementbyId("submit").addEventListener("click", myFunction):
+
+function myFuction(){
+	alert("Quiz submitted!")
+}
