a9e4385458444224fa542c9f1002e5edf929d116
jungin4ever <leeji4ever@gmail.com>
2024-03-23 13:17:58 +0900

    Added CSS

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -2,8 +2,19 @@ document.querySelector("h2").addEventListener("click", () => {
   alert("Ouch! Stop poking me!");
 });
 
-document.getElementbyId("submit").addEventListener("click", myFunction):
+s = document.getElementById("submit");
+s.addEventListener("click", myFunction);
+s.addEventListener("mouseover", myFunction2);
+s.addEventListener("mouseout", myFunction3);
 
-function myFuction(){
+function myFunction2(){
+	this.value = "Submit!"
+}
+
+function myFunction3(){
+	this.value = "Submit"
+}
+
+function myFunction(){
 	alert("Quiz submitted!")
 }
