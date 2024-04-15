3391c10c75ff606f91df5485ee33c474a7392451
jungin4ever <leeji4ever@gmail.com>
2024-04-16 08:56:56 +0900

    Dictionary CSV and Factbook

--- csv file python.py
+++ csv file python.py
@@ -9,7 +9,8 @@ with open("IDD_06042024052136105.csv") as f:
           yearIndex = header.index('Year')
           valueIndex = header.index('Value')
           countryIndex = header.index('Country')
-          
+          ageIndex = header.index('AGE')
+          currencyIndex = header.index('Unit')
           print("Using Indices", measureIndex, yearIndex, valueIndex)
           
           
@@ -17,20 +18,25 @@ with open("IDD_06042024052136105.csv") as f:
           yearTemp = 0
           countryTemp = listRows[rowCount][countryIndex]
           yearTemp = int(listRows[rowCount][yearIndex])
+
+          ectotal = {}
           
           for row in listRows:
+              current_countries = {}
               measure = row[measureIndex]
+              age = row[ageIndex]
+              currency = row[currencyIndex]
               rowCount+=1
-              if measure == "ECTOTAL":
+              if measure == "ECTOTAL" and age == 'TOT':
                   if countryTemp == row[countryIndex]:
                       if int(row[yearIndex]) >= yearTemp:
                           yearTemp = int(row[yearIndex])
                           value = row[valueIndex]
                   else:
-                      print("For", countryTemp, end='')
-                      print(" the most recent year", yearTemp,"'s value is ", end='')
-                      print(value)
-                      print("\n")
+                      current_countries['year'] = yearTemp
+                      current_countries['ectotal'] = value
+                      current_countries['currency'] = currency
+                      ectotal[countryTemp] = current_countries
                       countryTemp = row[countryIndex]
               
            
---
+++ world factbook parsing literacy.py
@@ -0,0 +1,43 @@
+import urllib3
+resp = urllib3.request("GET", "https://www.cia.gov/the-world-factbook/field/literacy")
+resp.data
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(resp.data, 'html.parser')
+
+
+
+literacy_blocks = soup.find_all(class_="pb30")
+
+
+i=0
+
+countries = {}
+for tag in literacy_blocks:
+    #print(type(tag))
+    h3 = tag.find("h3")
+    if h3 is None:
+        continue
+    a = h3.find("a")
+    
+    text = a.contents[0]
+    print(text)
+    strongs = tag.find_all("strong")
+    current_country = {}
+    for strong in strongs:
+        key = strong.contents[0].replace(':', '')
+        value = str(strong.next_sibling)
+        print(value)
+        year = ''
+        if "%" in value:
+            index = value.index("%")
+            if(index >=0):
+                value = value[:index+1]
+                year = value[index+1:]
+                #print(year)
+        print('key:', key, 'value:', value)
+        current_country[key] = value
+    countries[text] = current_country
+    i+=1
+    if i>10:
+        break
