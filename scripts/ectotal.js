80d38ca241529394b35f7076df4daa00ed61c095
jungin4ever <leeji4ever@gmail.com>
2024-05-12 10:33:04 +0900

    ectotal.js

--- scripts/ectotal.js
+++ scripts/ectotal.js
@@ -1,10 +1,12 @@
-var ectotal = require('./ectotal.json');
+
 console.log(ectotal)
 
 const text = $("#target").text();
 
 var random = Math.floor(Math.random()*ectotal.length)
 
-var txt = keyEctotalectotal[random]
+const keyEctotal = Object.keys(ectotal)
 
-$('#asdf').text('hello')
+var txt = keyEctotal[random]
+
+$('#asdf').text(txt);
