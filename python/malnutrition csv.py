5b4e3b00342a693ab185f990f48d38e5d742fbf2
jungin4ever <leeji4ever@gmail.com>
2024-06-23 05:18:10 +0900

    quiz update

---
+++ python/malnutrition csv.py
@@ -0,0 +1,96 @@
+import csv
+import random
+import json
+
+with open("../death-rate-from-malnutrition-ghe.csv") as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    print(header)
+    percents = {} #Death rate per 100,000 people
+    for row in reader:
+        country = row[0]
+        year = row[2]
+        percentage = float(row[3])
+        percents[country] = percentage, year
+    print(len(percents))
+
+def jiggle(value, amount=30):
+    newvalue = value + random.uniform(-amount, amount)
+    return max(newvalue,0)
+
+def generate_choices(value):
+    wrongchoice = []
+    usedchoice = set()
+
+    if value == 0:
+        wrongchoice = [
+            jiggle(value + 0.5,0.1), jiggle(value + 1,0.1), jiggle(value + 1.5,0.1),
+            jiggle(value - 0.5,0.1), jiggle(value - 1,0.1), jiggle(value - 1.5,0.1)]
+    elif value < 0.5:
+        wrongchoice = [
+            jiggle(value + 0.25,0.2), jiggle(value + 0.5,0.2), jiggle(value + 0.75,0.2),
+            jiggle(value - 0.25,0.2), jiggle(value - 0.5,0.2), jiggle(value - 0.75,0.2)]
+    elif value < 4:
+        wrongchoice = [
+            jiggle(value + 2,0.3), jiggle(value + 2.5,0.3), jiggle(value + 3,0.3),
+            jiggle(value - 2,0.3), jiggle(value - 2.5,0.3), jiggle(value - 3,0.3)]
+    else:
+        wrongchoice = [
+            jiggle(value*1.5,0.3), jiggle(value*2,0.3), jiggle(value*2.5,0.3),
+            jiggle(value/1.5,0.3), jiggle(value/2,0.3), jiggle(value/2.5,0.3)]
+    
+    random.shuffle(wrongchoice)
+
+    while len(wrongchoice) < 3:
+        newchoice = jiggle(value,0.3)
+        if newchoice != value and newchoice not in usedchoice:
+            wrongchoice.append(newchoice)
+            usedchoice.add(newchoice)
+
+    while len(set(wrongchoice)) < 3:
+        for c in range(len(wrongchoice)):
+            if wrongchoice.count(wrongchoice[i]) > 1:
+                wrongchoice[i] = jiggle(value, 0.3)
+
+    if value > 4:
+        wrongchoice.append(0)
+    
+    return wrongchoice[:3]
+
+num_questions = 10
+score = 0
+'''
+for x in range(num_questions):
+    length = len(percents)
+    r = random.randint(0,length-1)
+    key = list(percents)[r] #a country-year pair
+    value = percents[key] #the % of people with malnutrition
+
+    letters = ["A", "B", "C", "D"]
+    correctchoice = value
+    wrongchoice = generate_choices(value)
+    choices = [correctchoice] + wrongchoice
+
+    li = [0,1,2,3]
+    random.shuffle(li) #li will denote what index to go into in choices
+    print("What was the death rate per 100,000 people per year from malnutrition","in",key[0],"in",key[1],"?")
+    for i in range(4):
+        if li[i] == 0:  # This is the correct answer choice
+            print(f"{letters[i]}: {choices[0]:.2f}")
+        else:
+            print(f"{letters[i]}: ", end="")
+            print(f"{choices[li[i]]:.2f}")
+
+    answer = input().upper()
+    index = letters.index(answer)
+    if li[index]==0:
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect.")
+
+print(f"\nYour final score is {score} out of {num_questions}.")
+'''
+with open("../json/malnutrition.json",'w') as f:
+    f.write("var malnutrition = ")
+    json.dump(percents,f, indent=2)
---
+++ scripts/malnutrition.js
@@ -0,0 +1,213 @@
+//console.log(mortalityRate)
+
+
+//console.log(ectotal)
+
+ansArray = [];
+const keyMalnutrition = Object.keys(malnutrition) //makes a list consisted of the keys(countries) in mortalityRate
+for (let i=1;i<=10;i++){
+
+
+var random = Math.floor(Math.random()*keyMalnutrition.length)
+console.log(random)
+
+//console.log(keyEctotal)
+var txt = keyMalnutrition[random] //assigns a random country to ranCountry
+var ranYear = malnutrition[txt][1]
+//console.log(txt)
+$('#country'+i).text(txt) //use jquery to show random country in html
+$('#year'+i).text(ranYear)
+	
+	
+var random = ansRandom();
+console.log(random)
+var correctAns;
+
+
+
+if (random == 0){
+	console.log("asdf")
+	
+	var correctAns = highestAns(i);
+	ansArray.push(correctAns);
+	console.log(ansArray)
+}
+else if(random == 1){
+	console.log("fdsa")
+	
+	var correctAns = secondHighestAns(i);
+	ansArray.push(correctAns);
+	console.log(ansArray)
+}
+else if(random ==2){
+	
+	var correctAns = secondLowestAns(i);
+	ansArray.push(correctAns);
+	console.log(ansArray)
+}
+else if(random==3){
+	
+	var correctAns = lowestAns(i);
+	ansArray.push(correctAns);
+	console.log(ansArray)
+}
+}
+
+
+
+
+function shuffle(array) {
+	  let currentIndex = array.length;
+
+	  // While there remain elements to shuffle...
+	  while (currentIndex != 0) {
+
+		// Pick a remaining element...
+		let randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+		  array[randomIndex], array[currentIndex]];
+	  }
+	}
+
+
+function highestAns(num){
+	var ans = parseFloat(malnutrition[txt][0]) //gets the malnutrition rate
+	//var ansCurrency = ectotal[txt][2] //gets the currency
+	console.log(ans)
+	var ansArray = [num+'A',num+'B',num+'C',num+'D']
+	
+	
+	shuffle(ansArray)
+	console.log(ansArray[0])
+	
+	$("label[for*="+ansArray[0]+"]").text((ans).toFixed(2)+ " %")
+	$("label[for*="+ansArray[1]+"]").text((0.8*ans).toFixed(2)+ " %") 
+	$("label[for*="+ansArray[2]+"]").text((0.5*ans).toFixed(2)+ " %") 
+	$("label[for*="+ansArray[3]+"]").text((0.3*ans).toFixed(2)+ " %") 
+	
+	return ansArray[0]
+}
+
+function lowestAns(num){
+	var ans = parseFloat(malnutrition[txt][0])
+	//var ansCurrency = ectotal[txt][2]
+	var ansArray = [num+'A',num+'B',num+'C',num+'D']
+	
+	
+	shuffle(ansArray)
+	console.log(ans)
+	$("label[for*="+ansArray[0]+"]").text((2.1*ans).toFixed(2)+ " %") 
+	$("label[for*="+ansArray[1]+"]").text((1.5*ans).toFixed(2)+ " %") 
+	$("label[for*="+ansArray[2]+"]").text((1.3*ans).toFixed(2)+ " %") 
+	$("label[for*="+ansArray[3]+"]").text((ans).toFixed(2)+ " %")
+	
+	return ansArray[3]
+}
+
+function secondHighestAns(num){
+	var ans = parseFloat(malnutrition[txt][0])
+	//var ansCurrency = ectotal[txt][2]
+	var ansArray = [num+'A',num+'B',num+'C',num+'D']
+	
+	
+	shuffle(ansArray)
+	console.log(ans)
+	$("label[for*="+ansArray[0]+"]").text((1.6*ans).toFixed(2)+ " %") 
+	$("label[for*="+ansArray[1]+"]").text((ans).toFixed(2)+ " %")
+	$("label[for*="+ansArray[2]+"]").text((0.8*ans).toFixed(2)+ " %") 
+	$("label[for*="+ansArray[3]+"]").text((0.3*ans).toFixed(2)+ " %")
+		
+	return ansArray[1]
+}
+
+function secondLowestAns(num){
+	var ans = parseFloat(malnutrition[txt][0])
+	//var ansCurrency = ectotal[txt][2]
+	var ansArray = [num+'A',num+'B',num+'C',num+'D']
+	
+	
+	shuffle(ansArray)
+	console.log(ans)
+	$("label[for*="+ansArray[0]+"]").text((1.7*ans).toFixed(2)+ " %") 
+	$("label[for*="+ansArray[1]+"]").text((1.3*ans).toFixed(2)+ " %")
+	$("label[for*="+ansArray[2]+"]").text((ans).toFixed(2)+ " %")	
+	$("label[for*="+ansArray[3]+"]").text((0.6*ans).toFixed(2)+ " %") 
+	
+	return ansArray[2]
+}
+
+function ansRandom(){
+	var random = Math.floor(Math.random()*4)
+	return random
+}
+
+
+
+
+s = document.getElementById("submit");
+s.addEventListener("click", submitClick);
+s.addEventListener("mouseover", submitHover);
+s.addEventListener("mouseout", submitDefault);
+
+c = document.getElementById("restart");
+c.addEventListener("click", restart);
+
+var score = 0;
+var checClick = false;
+function submitHover(){
+	this.value = "Submit!"
+}
+
+function submitDefault(){
+	this.value = "Submit"
+}
+
+function submitClick(){
+	console.log($('#1B'))
+	console.log($('#1B')[0])
+	/*checClick = true;*/
+	s.style.display = 'none';
+	
+	if(document.getElementById(ansArray[0]).checked) {
+		score++;
+	}
+	
+	if(document.getElementById(ansArray[1]).checked) {
+		score++;
+	}
+	
+	if(document.getElementById(ansArray[2]).checked) {
+		score++;
+	}
+	if(document.getElementById(ansArray[3]).checked) {
+		score++;
+	}
+	if(document.getElementById(ansArray[4]).checked) {
+		score++;
+	}
+	if(document.getElementById(ansArray[5]).checked) {
+		score++;
+	}
+	if(document.getElementById(ansArray[6]).checked) {
+		score++;
+	}
+	if(document.getElementById(ansArray[7]).checked) {
+		score++;
+	}
+	if(document.getElementById(ansArray[8]).checked) {
+		score++;
+	}
+	if(document.getElementById(ansArray[9]).checked) {
+		score++;
+	}
+	
+	alert("Quiz submitted! "+ score+"/10 was correct")
+	alert("Restart the test.")
+}
+
+function restart(){
+	location.reload();
+}
