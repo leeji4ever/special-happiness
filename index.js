af8063e84a7d3b7efeb45ec4c6596c97cbb1ba30
jungin4ever <leeji4ever@gmail.com>
2024-07-20 10:37:45 +0900

    deleted index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const http = require("http"); 
-//create a server object: 
-http 
-  .createServer(function (req, res) { 
-    res.write("<h1>Hello World!</h1>");  
-    //write a response to the client 
-     
-    res.end();  
-    //end the response 
-  }) 
-  .listen(8080);  
-//Server runs on localhost:8080 
-
 // Import essential libraries 
 const express = require('express'); 
 const app = express(); 
