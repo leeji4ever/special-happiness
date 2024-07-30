28c298ab78e9dca7cc8054a9df9745e62a0f5dd2
jungin4ever <leeji4ever@gmail.com>
2024-07-31 01:49:14 +0900

    about update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ console.log('Running at Port 3000');
 
 // Setting EJS as the view engine
 app.set('view engine', 'ejs');
-
+app.set('trust proxy', true);
 app.get('/', (req, res) => {
     res.render('literacy quiz');
 	console.log(req.ip);
@@ -36,7 +36,7 @@ app.get('/views/:name', (req, res) => {
     res.render(req.params.name);
 });
 
-
+app.use('/images',express.static(__dirname +'/images'));
 app.use('/css',express.static(__dirname +'/css'));
 app.use('/scripts',express.static(__dirname +'/scripts'));
 app.use('/json',express.static(__dirname +'/json'));
